fcc15f4866e5526cc5f9e79312f7b5031593c5be
ciara <g00402357@atu.ie>
2024-10-18 18:35:28 +0100

    Added validation and updated service class

--- src/main/java/ie/atu/week5/customerapp/Customer.java
+++ src/main/java/ie/atu/week5/customerapp/Customer.java
@@ -4,12 +4,21 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
 @Data
 @Document(collection = "customers")
 public class Customer {
 
     @Id
+    @Min(value = 1, message = "postive and greater than 1")
     private String id;
+
+    @NotBlank(message= "Name must not be blank")
     private String name;
+
+    @Email(message = "Email must be valid")
     private String email;
 }
